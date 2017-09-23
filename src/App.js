d6129eea1c387121d3938dbc0fec043d2347d777
ninjafx <bilal.islam815@gmail.com>
2017-09-23 16:03:55 +0300

    fixed

--- src/App.js
+++ src/App.js
@@ -9,10 +9,9 @@ const App = () => (
       <h2>Welcome to React</h2>
     </div>
     <p className="App-intro">
-    To get started, edit <code>src/App.js</code> and save to reload.
+      To get started, edit <code>src/App.js</code> and save to reload.
     </p>
   </div>
 );
-
 
 export default App;
